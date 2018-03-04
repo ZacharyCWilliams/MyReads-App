ea334b71ac83a649ef3331d1b6f14b6fa98aa5b0
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-03-04 01:35:08 -0800

    Create CurrentlyReading.js

---
+++ src/CurrentlyReading.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './App.css'
+import Shelf from './Shelf'
+
+class CurrentlyReading extends Component {
+  render() {
+   return (
+		<div>
+			<Shelf />
+        </div>
+     )
+  }
+}
+
+export default CurrentlyReading
