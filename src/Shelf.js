51e35daf11f6fbddb4469bbbfaf2257f6538ab8e
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-03-16 17:35:17 -0700

    Update Shelf.js

--- src/Shelf.js
+++ src/Shelf.js
@@ -16,7 +16,7 @@ class Shelf extends Component {
 		<div className="book-top">
 		<div className="book-cover" key={book.title} style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
 		<div className="book-shelf-changer">
-		   <select id="select-shelf" onChange={(event) => this.props.updateShelf(event)}>
+		   <select id="select-shelf" key={book.id} onChange={(event) => this.props.updateShelf(event)}>
 		   <option value="none" disabled>Move to...</option>
 		   <option value="currentlyReading">Currently Reading</option>
 		   <option value="wantToRead">Want to Read</option>
