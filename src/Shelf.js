177e6eea1f3c0bc2685a03af72c7602dddeb3ea0
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-03-17 17:06:41 -0700

    Extract Book From Shelf
    - Adding Book Component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import App from './App'
+import App from './App';
+import Book from './Book';
 
 class Shelf extends Component {
 
@@ -10,26 +11,7 @@ class Shelf extends Component {
 	  <h2 className="bookshelf-title">{this.props.shelfName}</h2>
 	  <div className="bookshelf-books">
 	    <ol className="books-grid">
-		{this.props.currentShelf.map((book) => (
-		<li className="book-list-item">
-	     	<div className="book">
-		<div className="book-top">
-		<div className="book-cover" key={book.title} style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-		<div className="book-shelf-changer">
-		   <select id="select-shelf" key={book.id} onChange={(event) => this.props.updateShelf(event)}>
-		   <option value="none" disabled>Move to...</option>
-		   <option value="currentlyReading">Currently Reading</option>
-		   <option value="wantToRead">Want to Read</option>
-		   <option value="read">Read</option>
-		   <option value="none">None</option>
-		   </select>
-		</div>
-		</div>
-		<div className="book-title">{book.title}</div>
-		<div className="book-authors">{book.authors}</div>
-		</div>
-		</li>
-	       ))}
+		    <Book currentShelf={this.props.currentShelf} updateShelf={this.props.updateShelf}/>
 	    </ol>
 	  </div>
 	</div>
