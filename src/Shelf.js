9444b9c5146686914bebdc6c0a72628d640c2523
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-03-18 13:24:43 -0700

    Update Shelf.js

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import App from './App';
 import Book from './Book';
+import './App.css'
 
 class Shelf extends Component {
 
@@ -11,12 +12,13 @@ class Shelf extends Component {
 	  <h2 className="bookshelf-title">{this.props.shelfName}</h2>
 	  <div className="bookshelf-books">
 	    <ol className="books-grid">
-		    <Book currentShelf={this.props.currentShelf} updateShelf={this.props.updateShelf}/>
+		<Book currentShelf={this.props.currentShelf} updateShelf={this.props.updateShelf}/>
+		{console.log('this.props.currentShelf: ', this.props.currentShelf)}
 	    </ol>
 	  </div>
 	</div>
-   )
-  }
+	)
+    }
 }
 
 export default Shelf
