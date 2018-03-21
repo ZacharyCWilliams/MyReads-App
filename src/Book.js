8b9af87a690777f0f64551c4cd56f94ffde1f4d5
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-03-21 14:54:54 -0700

    Latest Version

--- src/Book.js
+++ src/Book.js
@@ -7,28 +7,28 @@ class Book extends Component {
 
   render() {
 
-  {console.log('book props: ', this.props)}
+    const bookIDProp = this.props.book.id
+    console.log('bookIDProp: ', bookIDProp)
+
    return (
-    <div className="books-grid">
-      <li className="book-list-item">
-        <div className="book">
-          <div className="book-top">
-          <div className="book-cover" key={book.title} style={{width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-          <div className="book-shelf-changer">
-            <select id="select-shelf" key={book.id} onChange={(event) => this.props.updateShelf(event)}>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-            </select>
-          </div>
-          </div>
-          <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
+    <li className="book-list-item">
+      <div className="book">
+        <div className="book-top">
+        <div className="book-cover" key={this.props.book.imageLinks.thumbnail} style={{width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
+        <div className="book-shelf-changer">
+          <select id="select-shelf" key={this.props.book.id} onChange={this.props.componentDidUpdate}>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+          </select>
         </div>
-      </li>
-    </div>
+        </div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.authors}</div>
+      </div>
+    </li>
    )
   }
 }
