efd5c55b9ee3664f44d947da8b4f8729977ac002
ZacharyCWilliams <34009260+ZacharyCWilliams@users.noreply.github.com>
2018-04-12 14:09:30 -0700

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -6,8 +6,6 @@ import './App.css'
 class Book extends Component {
 
   render() {
-    const bookIDProp = this.props.book.id
-
    return (
     <li className="book-list-item" key={this.props.book.id}>
       <div className="book">
@@ -15,7 +13,7 @@ class Book extends Component {
         <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
         <div className="book-shelf-changer">
           <select value={this.props.book.shelf} id="select-shelf" onChange={(event) => {this.props.updateShelf(this.props.book, event)}}>
-          <option value="none" disabled>Move to...</option>
+          <option disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
